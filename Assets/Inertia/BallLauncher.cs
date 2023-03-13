48b106a793064448d5b5d3c1e48c9b44c038f509
Keijiro Takahashi <keijiro@gmail.com>
2023-03-13 16:59:56 +0900

    Inertia test

---
+++ Assets/Inertia/BallLauncher.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+sealed class BallLauncher : MonoBehaviour
+{
+    [SerializeField] Rigidbody _prefab = null;
+    [SerializeField] float _speed = 10;
+
+    void Launch()
+    {
+        var rb = Instantiate(_prefab, transform.position, transform.rotation);
+        rb.velocity = new Vector3(0, 0, _speed);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Launch();
+    }
+}
